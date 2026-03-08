617ed0e70d61fd03e8c5aa1b45c7706be08e3000
Mohammed Swalih N K <swalink555@gmail.com>
2026-03-08 17:36:08 +0530

    vercel deployment

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import jsonServer from 'json-server'
+
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+server.use(router)
+
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => {
+  console.log(`JSON Server running on port ${PORT}`)
+})
